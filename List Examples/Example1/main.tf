7f203a9065fc09b6827c9fa8ed0b940a70f96f4f
Pankaj Singh <47411477+pankisingh@users.noreply.github.com>
2021-10-07 12:07:08 +0530

    added output for print the list of element

--- List Examples/Example1/main.tf
+++ List Examples/Example1/main.tf
@@ -1,3 +1,13 @@
 resource "random_pet" "my-list" {
  prefix  = var.prefix[0]
 }
+
+#Print the first elment of list..
+
+output "name"{
+ value = var.prefix[0]
+}
+
+output "my-name"{
+ value = random_pet.my-list.prefix
+}
