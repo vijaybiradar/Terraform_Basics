6f5a7cda341d1b85b05087bd58327ac2fcfcb232
vijaybiradar <38376802+vijaybiradar@users.noreply.github.com>
2022-03-19 13:04:04 +0530

    Update main.tf

--- List Examples/Example1/main.tf
+++ List Examples/Example1/main.tf
@@ -1,3 +1,8 @@
+## Create a directory name terraform and get inside it
+mkdir terraform && cd terraform
+## Create main.tf under terraform directory
+vi main.tf
+
 resource "random_pet" "my-list" {
  prefix  = var.prefix[0]
 }
@@ -11,3 +16,27 @@ output "name"{
 output "my-name"{
  value = random_pet.my-list.prefix
 }
+## Format the code
+terraform fmt
+## List files (Terraform stores the workspace states in a directory called terraform.tfstate.d)
+ls -ltrh #terraform created a new directory terraform.tfstate.d
+ls terraform.tfstate.d/ #terraform created a new directory test inside terraform.tfstate.d
+ 
+## Initialize the provider
+terraform init
+## List files (Terraform stores the workspace states in a directory called terraform.tfstate.d)
+ls -ltrh #terraform created a new directory terraform.tfstate.d
+ls terraform.tfstate.d/ #terraform created a new directory test inside terraform.tfstate.d
+ 
+## Plan the changes
+terraform plan
+ 
+## Apply the changes
+terraform apply
+ 
+## Show state details
+terraform show
+ 
+## Cleanup
+## Destory resources
+terraform destory
