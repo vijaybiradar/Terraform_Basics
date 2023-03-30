6d4cf5427f27832b75838589c315c7b35fb621bc
vijaybiradar <38376802+vijaybiradar@users.noreply.github.com>
2023-03-30 15:49:27 +0530

    Update main.tf

--- Data Sources Examples/Example1/main.tf
+++ Data Sources Examples/Example1/main.tf
@@ -1,9 +1,12 @@
+# Define a local file resource that creates a file called "Hello.txt"
+
 resource "local_file" "hello" {
   filename = "Hello.txt"
   content = "Hello Vijay Biradar"
   file_permission = "0700"
 }
 
+# Define a data source that reads a file called "Hi.txt"
 data "local_file" "hi" {
   filename = "Hi.txt"
 }
