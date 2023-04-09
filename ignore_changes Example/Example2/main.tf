bb07becee5813e3d3f85c89e3d74483401d919ba
vijaybiradar <38376802+vijaybiradar@users.noreply.github.com>
2023-04-09 11:14:17 +0530

    Update main.tf

--- ignore_changes Example/Example2/main.tf
+++ ignore_changes Example/Example2/main.tf
@@ -8,3 +8,19 @@ resource "local_file" "hello" {
       
 }
 }
+
+
+or
+
+resource "local_file" "hello" {
+  filename        = "Hello.txt"
+  content         = "Hello Vijay Biradar"
+  file_permission = "0700"
+  
+  lifecycle {
+    ignore_changes = [
+      content,
+      file_permission,
+    ]
+  }
+}
