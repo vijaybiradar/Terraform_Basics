8b42e706fe677824e1dcc399f1b046ec29fafe51
vijaybiradar <38376802+vijaybiradar@users.noreply.github.com>
2023-09-10 19:03:14 +0530

    Create outputs.tf

---
+++ Map Examples/Example1/outputs.tf
@@ -0,0 +1,19 @@
+output "shuffled_nouns" {
+  value = random_shuffle.random_nouns.result
+}
+
+output "shuffled_adjectives" {
+  value = random_shuffle.random_adjectives.result
+}
+
+output "shuffled_verbs" {
+  value = random_shuffle.random_verbs.result
+}
+
+output "shuffled_adverbs" {
+  value = random_shuffle.random_adverbs.result
+}
+
+output "shuffled_numbers" {
+  value = random_shuffle.random_numbers.result
+}
