64e51f57c8353b81282cd1b1a320a9e029b33815
vijaybiradar <38376802+vijaybiradar@users.noreply.github.com>
2023-09-10 19:08:04 +0530

    Update variable.tf

--- Map Examples/Example1/variable.tf
+++ Map Examples/Example1/variable.tf
@@ -1,19 +1,14 @@
-resource "random_shuffle" "random_nouns" {
-   input = var.words["nouns"]
- }
-  
- resource "random_shuffle" "random_adjectives" {
-   input = var.words["adjectives"]
- }
-  
- resource "random_shuffle" "random_verbs" {
-     input = var.words["verbs"]
- }
-  
- resource "random_shuffle" "random_adverbs" {
-   input = var.words["adverbs"]
- }
-  
- resource "random_shuffle" "random_numbers" {
-   input = var.words["numbers"]
- }
+# variables.tf
+# Define variables for word categories and descriptions.
+
+variable "words" {
+  description = "A word pool to use for Mad Libs"
+  type        = map(list(string))
+  default = {
+    nouns      = ["army", "panther", "walnuts", "sandwich", "Zeus", "banana", "cat", "jellyfish", "jigsaw", "violin", "milk", "sun"]
+    adjectives = ["bitter", "sticky", "thundering", "abundant", "chubby", "grumpy"]
+    verbs      = ["run", "dance", "love", "respect", "kicked", "baked"]
+    adverbs    = ["delicately", "beautifully", "quickly", "truthfully", "wearily"]
+    numbers    = [42, 27, 101, 73, -5, 0]
+  }
+}
